1bb209f842af37663532cebefd8bc9cd02238072
Dagim Tesfaye <valt.tesfaye@gmail.com>
2023-03-07 19:13:19 +0300

    Changed made

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+ import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ function App() {
+     return( 
+      <div className = "App">
+        <Job salary = {90000} position="Senior SDE" company="Amazon"/>
+        <Job salary = {60000} position="Junior SDE" company="Google"/>
+        <Job salary = {70000} position="Project Manager" company="Netflix"/>
+      </div>
+     
+     );
+ }
 
-export default App;
+ const Job = (props) => {
+    return (
+        <div>
+            <h1>{props.salary}</h1>
+            <h1>{props.position}</h1>
+            <h1>{props.company}</h1>
+        </div>
+    )
+ }
+
+ export default App;
